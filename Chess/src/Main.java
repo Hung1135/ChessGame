0c1953908226d07904fa67e2ce0b358dcbfbb898
hungdev <23130126@st.hcmuaf.edu.vn>
2025-12-04 23:45:42 +0700

    fix issue

--- Chess/src/Main.java
+++ Chess/src/Main.java
@@ -1,15 +1,5 @@
-
-final class Main {
+public class Main {
     public static void main(String[] args) {
-        new GameFrame();
-    }
-
-    public class Main {
-        GameFrame gameframe;
-
-        public Main() {
-            Objects.requireNonNull(Main.this);
-            super();
-        }
+        GameFrame game = new GameFrame();
     }
 }
